507af9f8fbbf40da2fc8cde3a8da6d6c9370b2de
Andrew Baxter <asbaxter1@gmail.com>
2023-03-14 18:55:27 -0400

    stopped at Iran, adding more recipe data

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/data', (req, res) => {
-  fs.readFile('./data/recipes.json', (err, data) => {
+app.get("/data", (req, res) => {
+  fs.readFile("./data/recipes.json", (err, data) => {
     if (err) throw err;
     const jsonData = JSON.parse(data);
     res.json(jsonData);
@@ -15,7 +15,7 @@ app.get('/data', (req, res) => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Now on port ${PORT}!`);
